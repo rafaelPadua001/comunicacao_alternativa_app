bb946d386b80309a234815c2ef586b70149fd036
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-02 12:39:05 -0300

    Refact
    Refact card to add pictograms
    add acordion effect on add pictograms card.

--- lib/screens/admin/pictograms/addPictogram.dart
+++ lib/screens/admin/pictograms/addPictogram.dart
@@ -33,65 +33,80 @@ class _AddPictogramState extends State<AddPictogram> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add Pictogram")),
-      body: Center(
-        child: Card(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          elevation: 4,
-          child: Padding(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _image != null
-                    ? Image.file(_image!, height: 200)
-                    : Text('Select Image', style: TextStyle(fontSize: 16)),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: _pickImage,
-                  style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.file_upload, size: 24),
-                      SizedBox(width: 8),
-                      Text("Upload Image"),
-                    ],
-                  ),
+      appBar: AppBar(title: Text("Pictograms")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              elevation: 4,
+              child: ExpansionTile(
+                title: Text(
+                  "Add Pictogram",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                if (_image != null) ...[
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: _labelController,
-                    decoration: InputDecoration(labelText: "Pictogram Name"),
-                  ),
-                  SizedBox(height: 10),
-                  DropdownButton<String>(
-                    value: _selectedCategory,
-                    hint: Text("Select a category"),
-                    isExpanded: true,
-                    items: categories.map((String category) {
-                      return DropdownMenuItem<String>(
-                        value: category,
-                        child: Text(category),
-                      );
-                    }).toList(),
-                    onChanged: (String? newCategory) {
-                      setState(() {
-                        _selectedCategory = newCategory;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: _savePictogram,
-                    child: Text('Save'),
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _image != null
+                            ? Image.file(_image!, height: 200)
+                            : Text('Select Image', style: TextStyle(fontSize: 16)),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: _pickImage,
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.file_upload, size: 24),
+                              SizedBox(width: 8),
+                              Text("Upload Image"),
+                            ],
+                          ),
+                        ),
+                        if (_image != null) ...[
+                          SizedBox(height: 10),
+                          TextField(
+                            controller: _labelController,
+                            decoration: InputDecoration(labelText: "Pictogram Name"),
+                          ),
+                          SizedBox(height: 10),
+                          DropdownButton<String>(
+                            value: _selectedCategory,
+                            hint: Text("Select a category"),
+                            isExpanded: true,
+                            items: categories.map((String category) {
+                              return DropdownMenuItem<String>(
+                                value: category,
+                                child: Text(category),
+                              );
+                            }).toList(),
+                            onChanged: (String? newCategory) {
+                              setState(() {
+                                _selectedCategory = newCategory;
+                              });
+                            },
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: _savePictogram,
+                            child: Text('Save'),
+                          ),
+                        ],
+                      ],
+                    ),
                   ),
                 ],
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
