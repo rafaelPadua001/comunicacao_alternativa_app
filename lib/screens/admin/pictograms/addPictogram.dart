f868ee3dbde94f671d908ff49e75a95d2a5ae1fd
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-05 00:11:03 -0300

    Fix
    Fix images supabase and local show
    Create functions to tap or long tap
    create Icon to delete itens.
    create deleteSelectedItems function to remove itens.

--- lib/screens/admin/pictograms/addPictogram.dart
+++ lib/screens/admin/pictograms/addPictogram.dart
@@ -22,12 +22,15 @@ class _AddPictogramState extends State<AddPictogram> {
     "Fruit",
     "Drink",
   ];
-  
+
   bool _isUploading = false;
   bool _isLoading = true;
+  bool _isSelecting = false;
+  Set<int> _selectedIndex = {};
+
   List<Map<String, dynamic>> _allPictograms = [];
   final localPictogramStorage = localPictogram.Pictogramstorage();
-  
+
   Future<void> _pickImage() async {
     final pickedFile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
@@ -55,7 +58,8 @@ class _AddPictogramState extends State<AddPictogram> {
 
     try {
       // Upload da imagem para o Supabase Storage
-      final String? imageUrl = await adminPictogram.PictogramStorage.uploadImage(_image!);
+      final String? imageUrl = await adminPictogram
+          .PictogramStorage.uploadImage(_image!);
       if (imageUrl == null) throw Exception("Falha no upload da imagem");
 
       // Salva os dados no banco
@@ -87,47 +91,79 @@ class _AddPictogramState extends State<AddPictogram> {
     }
   }
 
- Future<void> _loadPictograms() async {
-  setState(() => _isLoading = true);
-  try {
-    // Carregar pictogramas locais
-    List<Pictogram> localPictograms = await localPictogramStorage.getPictograms();
-
-    // Converter os pictogramas locais de List<Pictogram> para List<Map<String, dynamic>>
-    List<Map<String, dynamic>> localPictogramsMap = localPictograms.map((pictogram) {
-      return {
-        "isLocal": true,
-        "imagePath": pictogram.imagePath,  // Verifique se esse caminho está correto
-        "label": pictogram.label,
-      };
-    }).toList();
-
-    // Carregar pictogramas do Supabase
-    var supabasePictograms = await adminPictogram.PictogramStorage.getPictograms();
-    print("Pictogramas do Supabase: $supabasePictograms");  // Verifique o tipo real da resposta
-
-    // Certifique-se de que a resposta está no formato esperado
-    List<Map<String, dynamic>> supabasePictogramsMap = supabasePictograms.map((pictogram) {
-      return {
-        "isLocal": false,
-        "imageUrl": pictogram.imageUrl, // Garanta que você está acessando a chave correta
-        "label": pictogram.label,
-      };
-    }).toList();
-
+  void _toggleSelectionMode(int index) {
     setState(() {
-      _allPictograms = [];
-      _allPictograms.addAll(localPictogramsMap);
-      _allPictograms.addAll(supabasePictogramsMap);
-      _isLoading = false;
+      if (_isSelecting) {
+        if (_selectedIndex.contains(index)) {
+          _selectedIndex.remove(index);
+          if (_selectedIndex.isEmpty) {
+            _isSelecting = false;
+          }
+        } else {
+          _selectedIndex.add(index);
+        }
+      }
     });
-  } catch (e) {
-    print("Erro ao carregar pictogramas: $e");
-    setState(() => _isLoading = false);
   }
-}
 
+  void _startSelection(int index) {
+    setState(() {
+      _isSelecting = true;
+      _selectedIndex.add(index);
+    });
+  }
 
+  void  _deleteSelectedItems(Set<int> index){
+    print(index);
+  }
+  Future<void> _loadPictograms() async {
+    setState(() => _isLoading = true);
+    try {
+      // Carregar pictogramas locais
+      List<Pictogram> localPictograms =
+          await localPictogramStorage.getPictograms();
+
+      // Converter os pictogramas locais de List<Pictogram> para List<Map<String, dynamic>>
+      List<Map<String, dynamic>> localPictogramsMap =
+          localPictograms.map((pictogram) {
+            return {
+              "isLocal": true,
+              "imagePath":
+                  pictogram.imagePath, // Verifique se esse caminho está correto
+              "label": pictogram.label,
+            };
+          }).toList();
+
+      // Carregar pictogramas do Supabase
+      var supabasePictograms =
+          await adminPictogram.PictogramStorage.getPictograms();
+      print(
+        "Pictogramas do Supabase: $supabasePictograms",
+      ); // Verifique o tipo real da resposta
+
+      // Certifique-se de que a resposta está no formato esperado
+      List<Map<String, dynamic>> supabasePictogramsMap =
+          supabasePictograms.map((pictogram) {
+            return {
+              "isLocal": false,
+              "imageUrl":
+                  pictogram
+                      .imageUrl, // Garanta que você está acessando a chave correta
+              "label": pictogram.label,
+            };
+          }).toList();
+
+      setState(() {
+        _allPictograms = [];
+        _allPictograms.addAll(localPictogramsMap);
+        _allPictograms.addAll(supabasePictogramsMap);
+        _isLoading = false;
+      });
+    } catch (e) {
+      print("Erro ao carregar pictogramas: $e");
+      setState(() => _isLoading = false);
+    }
+  }
 
   @override
   void initState() {
@@ -164,9 +200,9 @@ class _AddPictogramState extends State<AddPictogram> {
                           _image != null
                               ? Image.file(_image!, height: 200)
                               : Text(
-                                  'Select Image',
-                                  style: TextStyle(fontSize: 16),
-                                ),
+                                'Select Image',
+                                style: TextStyle(fontSize: 16),
+                              ),
                           SizedBox(height: 10),
                           ElevatedButton(
                             onPressed: _pickImage,
@@ -192,12 +228,13 @@ class _AddPictogramState extends State<AddPictogram> {
                               value: _selectedCategory,
                               hint: Text("Select a category"),
                               isExpanded: true,
-                              items: categories.map((String category) {
-                                return DropdownMenuItem<String>(
-                                  value: category,
-                                  child: Text(category),
-                                );
-                              }).toList(),
+                              items:
+                                  categories.map((String category) {
+                                    return DropdownMenuItem<String>(
+                                      value: category,
+                                      child: Text(category),
+                                    );
+                                  }).toList(),
                               onChanged: (String? newCategory) {
                                 setState(() {
                                   _selectedCategory = newCategory;
@@ -218,41 +255,88 @@ class _AddPictogramState extends State<AddPictogram> {
               ),
               Card(
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(0),
                 ),
                 elevation: 4,
                 child: ExpansionTile(
-                  title: Text(
-                    "All Pictograms",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "All Pictograms",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      if (_selectedIndex.isNotEmpty)
+                        IconButton(
+                          onPressed: () {
+                            _deleteSelectedItems(_selectedIndex);
+                          },
+                          icon: Icon(Icons.delete, color: Colors.red),
+                        ),
+                    ],
                   ),
                   children: [
                     Padding(
                       padding: EdgeInsets.all(16),
-                      child: _isLoading
-                          ? CircularProgressIndicator()
-                          : _allPictograms.isEmpty
+                      child:
+                          _isLoading
+                              ? CircularProgressIndicator()
+                              : _allPictograms.isEmpty
                               ? Text("Nenhum pictograma encontrado.")
                               : Wrap(
-                                  spacing: 8.0,
-                                  runSpacing: 8.0,
-                                  children: _allPictograms.map((pictogram) {
-                                    return Column(
-                                      children: [
-                                        pictogram['isLocal']
-                                            ? Image.file(
-                                                File(pictogram['imagePath']),
-                                                height: 50,
-                                              )
-                                            : Image.network(
-                                                pictogram['imagePath'],
-                                                height: 50,
+                                spacing: 8.0,
+                                runSpacing: 8.0,
+                                children:
+                                    _allPictograms.asMap().entries.map((entry) {
+                                      int index = entry.key;
+                                      var pictogram = entry.value;
+                                      bool isSelected = _selectedIndex.contains(
+                                        index,
+                                      );
+
+                                      return GestureDetector(
+                                        onTap:
+                                            () => _toggleSelectionMode(index),
+                                        onLongPress:
+                                            () => _startSelection(index),
+                                        child: Column(
+                                          children: [
+                                            Container(
+                                              decoration: BoxDecoration(
+                                                border:
+                                                    isSelected
+                                                        ? Border.all(
+                                                          color: Colors.blue,
+                                                          width: 3.0,
+                                                        )
+                                                        : null,
+                                                borderRadius:
+                                                    BorderRadius.circular(80),
                                               ),
-                                        Text(pictogram['label']),
-                                      ],
-                                    );
-                                  }).toList(),
-                                ),
+                                              padding: EdgeInsets.all(2),
+                                              child:
+                                                  pictogram['isLocal']
+                                                      ? Image.file(
+                                                        File(
+                                                          pictogram['imagePath'],
+                                                        ),
+                                                        height: 50,
+                                                      )
+                                                      : Image.network(
+                                                        pictogram['imageUrl'],
+                                                        height: 50,
+                                                      ),
+                                            ),
+
+                                            Text(pictogram['label']),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                              ),
                     ),
                   ],
                 ),
