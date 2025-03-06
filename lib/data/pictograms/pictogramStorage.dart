db3525c4d4358f69da602ed4f8ecd8ce32c62564
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-06 00:58:02 -0300

    Feat
    Create class delte_pictograms to receive local pictograms and cloud pictograms.
    Create function to delete cloud pictograms.

--- lib/data/pictograms/pictogramStorage.dart
+++ lib/data/pictograms/pictogramStorage.dart
@@ -49,26 +49,71 @@ class PictogramStorage {
     }
   }
 
-static Future<List<Pictogram>> getPictograms() async {
-  try {
-    final response = await SupabaseConfig.supabase
-        .from('pictograms_table')
-        .select();
-    print(response);
-    // Verifique se a resposta contém a chave 'data'
-    if (response != null && response is List) {
-      List<Pictogram> pictograms = response
-          .map<Pictogram>((data) => Pictogram.fromJson(data))
-          .toList();
-      return pictograms;
-    } else {
-      print("Formato de resposta inválido ou vazio");
+  static Future<List<Pictogram>> getPictograms() async {
+    try {
+      final response =
+          await SupabaseConfig.supabase.from('pictograms_table').select();
+      print(response);
+      // Verifique se a resposta contém a chave 'data'
+      if (response != null && response is List) {
+        List<Pictogram> pictograms =
+            response
+                .map<Pictogram>((data) => Pictogram.fromJson(data))
+                .toList();
+        return pictograms;
+      } else {
+        print("Formato de resposta inválido ou vazio");
+        return [];
+      }
+    } catch (error) {
+      print("Erro ao buscar pictogramas: $error");
       return [];
     }
-  } catch (error) {
-    print("Erro ao buscar pictogramas: $error");
-    return [];
   }
+
+ static Future<void> deletePictograms(List<String> imageNames) async {
+  for (var image in imageNames) {
+    try {
+      final uri = Uri.parse(image);
+      final imagePath = uri.pathSegments.last;
+      final fullPath = 'pictograms/$imagePath';
+
+      final response = await SupabaseConfig.supabase
+          .from('pictograms_table')  // Certifique-se de que o nome da tabela está correto
+          .delete()
+          .eq('imageUrl', image)
+          .select();  // A consulta precisa retornar algo, pode ser uma confirmação
+
+      if (response == null) {
+        print('Erro ao tentar deletar a imagem $image: resposta nula');
+        continue;
+      }
+
+      // Verifique se houve algum erro
+      if (response.isEmpty) {
+        // Se a resposta estiver vazia, significa que nenhum registro foi deletado
+        print('Nenhum registro encontrado para a imagem $image');
+      } else {
+        // Se a resposta não estiver vazia, a deleção foi bem-sucedida
+        print('Imagem $image deletada com sucesso!');
+      }
+
+      final storageResponse = await SupabaseConfig.supabase.storage
+      .from('pictograms')
+      .remove([fullPath]);
+
+
+      if(storageResponse.isEmpty){
+        print('Erro ao tentar deletar a imagem $image do storage ${storageResponse}');
+      }
+      else{
+        print('$image removida do storage com sucesso');
+      }
+    } catch (e) {
+      print('Erro ao tentar deletar a imagem $image: $e');
+    }
+  }
+  print('Delete disparado');
 }
 
 }
---
+++ lib/services/delete_pictograms.dart
@@ -0,0 +1,49 @@
+import 'package:comunicacao_alternativa_app/screens/admin/pictograms/addPictogram.dart';
+import '../data/pictograms/pictogramStorage.dart' as adminPictograms;
+import '../data/pictogramStorage.dart' as localPictograms;
+
+
+class DeletePictograms {
+  final List<String> cloudImages;
+  final List<String> localImages;
+
+  DeletePictograms({
+    required this.cloudImages,
+    required this.localImages,
+  });
+
+  void deleteLocalImages(dynamic imageNames){
+    print('Image $imageNames removida do armazenamento local');
+  }
+
+  void deleteCloudImages(dynamic imageNames){
+    List<String> imagesToDelete = (imageNames is String) ? [imageNames] : List<String>.from(imageNames);
+
+    adminPictograms.PictogramStorage.deletePictograms(imagesToDelete);
+    for(var imageName in imagesToDelete){
+      print('Image $imageName removida da nuvem');
+    }
+      
+  }
+
+  void deleteAll(){
+     print("Todas as imagens foram removidas.");
+  }
+
+  void showImages(){
+    print('Images locais: $localImages');
+    print('Images cloud: $cloudImages');
+  }
+}
+
+
+// void main(){
+//    var addPictograms = AddPictogram();
+
+//   var pictograms = DeletePictograms(
+//     localImages: addPictograms.getLocalImages(),
+//     cloudImages: addPictograms.getCloudImages(),
+//   );
+
+//   pictograms.showImages();
+// }
