cbdb474184676fe70bdcdb0d23ea5751c852123d
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-07 12:23:25 -0300

    Fix
    Clear comments code on dashboard

--- lib/screens/admin/dashboard.dart
+++ lib/screens/admin/dashboard.dart
@@ -11,42 +11,6 @@ class DashboarAdmindScreen extends StatelessWidget {
     final user = SupabaseConfig.supabase.auth.currentUser;
 
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Admin Dashboard'),
-      //   // actions: <Widget>[
-      //   //   IconButton(
-      //   //     icon: const Icon(Icons.add_alert),
-      //   //     tooltip: 'Show Snackbar',
-      //   //     onPressed: () {
-      //   //       ScaffoldMessenger.of(context).showSnackBar(
-      //   //         const SnackBar(content: Text('This is a snackbar')),
-      //   //       );
-      //   //     },
-      //   //   ),
-      //   //   IconButton(
-      //   //     icon: const Icon(Icons.navigate_next),
-      //   //     tooltip: 'Go to the next page',
-      //   //     onPressed: () {
-      //   //       Navigator.push(
-      //   //         context,
-      //   //         MaterialPageRoute<void>(
-      //   //           builder: (BuildContext context) {
-      //   //             return Scaffold(
-      //   //               appBar: AppBar(title: const Text('Next page')),
-      //   //               body: const Center(
-      //   //                 child: Text(
-      //   //                   'This is the next page',
-      //   //                   style: TextStyle(fontSize: 24),
-      //   //                 ),
-      //   //               ),
-      //   //             );
-      //   //           },
-      //   //         ),
-      //   //       );
-      //   //     },
-      //   //   ),
-      //   // ],
-      // ),
       body: Row(
         children: [
           // Navegação lateral com o NavigatorRail
