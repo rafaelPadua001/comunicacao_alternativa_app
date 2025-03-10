8ffd6153a7f79c3ed3b349983d3cba5c7023d27d
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2025-03-10 17:23:19 -0300

    Feat
    Create student register
    Clean firebase functions

---
+++ lib/data/students/studentStorage.dart
@@ -0,0 +1,60 @@
+import 'package:comunicacao_alternativa_app/services/supabase_config.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../models/student/student.dart';
+
+class StudentStorage {
+  static Future<String?> registerUser(Student student, {String? phone}) async {
+    try {
+      final response = await SupabaseConfig.supabase.auth.signUp(
+        email: student.email,
+        password: 'example-password', //student.password,
+        phone: phone, // opcional, apenas se desejar usar o número de telefone
+      );
+
+      final Session? session = response.session;
+      final User? user = response.user;
+
+      if (user != null) {
+        final saveResult = await saveUser(student, user.id);
+
+        if (saveResult != null) {
+          return saveResult;
+        }
+
+        return null;
+      }
+    } on AuthException catch (e) {
+      print('Erro de autenticação: ${e.message}');
+      return e.message; // Retorna a mensagem de erro do Supabase
+    } catch (e) {
+      print('Erro inesperado ao registrar o aluno: $e');
+      return 'Ocorreu um erro inesperado. Tente novamente.';
+    }
+  }
+
+  static Future<String?> saveUser(Student student, String userId) async {
+    try {
+      // Insere os dados na tabela `user_profiles`
+      final response = await SupabaseConfig.supabase.from('user_profiles').insert({
+        'id': userId, // ID do usuário (herdado de auth.users.id)
+        'displayname': student.fullName,
+        'email': student.email, // E-mail do estudante
+        'usertype': 'student', // Tipo de usuário (fixo como "student")
+      });
+
+      // Sucesso
+      return null;
+    }
+    on PostgrestException catch (e) {
+     // Captura erros específicos do Supabase
+     print('Erro ao salvar usuário na tabela: $e');
+     return 'Erro ao salvar usuário na tabela: ${e.message}';
+    }
+    catch (e) {
+      // Captura outros erros
+      print('Erro ao salvar usuário na tabela: $e');
+      return 'Erro ao salvar usuário na tabela. Tente novamente.';
+    }
+  }
+}
--- lib/services/supabase_config.dart
+++ lib/services/supabase_config.dart
@@ -2,9 +2,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
   static const String supabaseUrl = "https://sauizwxtoqjeioawowvs.supabase.co";
-
   static const String supabaseAnonKey =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhdWl6d3h0b3FqZWlvYXdvd3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NjQxOTAsImV4cCI6MjA1NjU0MDE5MH0.CF7avcxCULdJq5NYc-Usv-WdWHodtdIAAcp_miCm0dk";
 
-  static final supabase = SupabaseClient(supabaseUrl, supabaseAnonKey);
+  // Acesse o cliente Supabase através do Supabase.instance.client
+  static SupabaseClient get supabase => Supabase.instance.client;
 }
